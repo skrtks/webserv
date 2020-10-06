ea5d306f5cb6c0096909cc96f0524973fba79708
Sam Kortekaas <samkortekaas@protonmail.com>
2020-10-06 13:19:43 +0200

    Added Process Req class

---
+++ ProcessRequest.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ProcessRequest.cpp                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sam <sam@student.codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/06 12:01:35 by sam           #+#    #+#                 */
+/*   Updated: 2020/10/06 12:01:35 by sam           ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ProcessRequest.hpp"
+
+ProcessRequest::ProcessRequest() {
+}
+
+ProcessRequest::~ProcessRequest() {
+}
+
+
---
+++ ProcessRequest.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ProcessRequest.hpp                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sam <sam@student.codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/06 12:01:32 by sam           #+#    #+#                 */
+/*   Updated: 2020/10/06 12:01:32 by sam           ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PROCESSREQUEST_HPP
+#define PROCESSREQUEST_HPP
+
+
+class ProcessRequest {
+public:
+	ProcessRequest();
+	virtual ~ProcessRequest();
+};
+
+
+#endif //PROCESSREQUEST_HPP
