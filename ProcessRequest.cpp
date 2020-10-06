668938446fecbab5d9185c39b7bf52aab3bb6bfd
Sam Kortekaas <samkortekaas@protonmail.com>
2020-10-06 19:45:39 +0200

    Work in progress

--- ProcessRequest.cpp
+++ ProcessRequest.cpp
@@ -13,9 +13,22 @@
 #include "ProcessRequest.hpp"
 
 ProcessRequest::ProcessRequest() {
+	_rawRequest = "GET /yosemite.jpg HTTP/1.1\n"
+				  "Host: localhost:8080\n"
+				  "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:81.0) Gecko/20100101 Firefox/81.0\n"
+				  "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n"
+				  "Accept-Language: nl,en-US;q=0.7,en;q=0.3\n"
+				  "Accept-Encoding: chunked\n"
+				  "DNT: 1\n"
+				  "Connection: keep-alive\n"
+				  "Upgrade-Insecure-Requests: 1\n"
+				  "Pragma: no-cache\n"
+				  "Cache-Control: no-cache\n";
 }
 
 ProcessRequest::~ProcessRequest() {
 }
 
+void ProcessRequest::parseRequest() {
 
+}
--- ProcessRequest.hpp
+++ ProcessRequest.hpp
@@ -13,11 +13,43 @@
 #ifndef PROCESSREQUEST_HPP
 #define PROCESSREQUEST_HPP
 
+#include <vector>
+#include <string>
+
+//Accept-Charsets ◦ Accept-Language ◦ Allow
+//◦ Authorization
+//◦ Content-Language ◦ Content-Length
+//◦ Content-Location ◦ Content-Type
+//◦ Date ◦ Host
+
+enum headerType {
+	ACCEPT_CHARSET,
+	ACCEPT_LANGUAGE,
+	ALLOW,
+	AUTHORIZATION,
+	CONTENT_LANGUAGE,
+	CONTENT_LENGTH,
+	CONTENT_LOCATION,
+	CONTENT_TYPE,
+	DATE,
+	HOST,
+	LAST_MODIFIED,
+	LOCATION,
+	REFERER,
+	RETRY_AFTER,
+	SERVER,
+	TRANSFER_ENCODING,
+	USER_AGENT,
+	WWW_AUTHENTICATE
+};
 
 class ProcessRequest {
+	std::vector<std::pair<headerType, std::string> > _headers;
+	std::string _rawRequest;
 public:
 	ProcessRequest();
 	virtual ~ProcessRequest();
+	void parseRequest();
 };
 
 
