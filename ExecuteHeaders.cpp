eb0782e3bd7bbd779b63734441c4ba3938eb70ae
skrtks <samkortekaas@protonmail.com>
2020-10-08 16:15:53 +0200

    Added ExecuteHeaders class

---
+++ ExecuteHeaders.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ExecuteHeaders.cpp                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: skorteka <skorteka@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/08 16:15:11 by skorteka      #+#    #+#                 */
+/*   Updated: 2020/10/08 16:15:11 by skorteka      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ExecuteHeaders.hpp"
+
+ExecuteHeaders::ExecuteHeaders() {
+}
+
+ExecuteHeaders::~ExecuteHeaders() {
+}
---
+++ ExecuteHeaders.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ExecuteHeaders.hpp                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: skorteka <skorteka@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/08 16:15:14 by skorteka      #+#    #+#                 */
+/*   Updated: 2020/10/08 16:15:14 by skorteka      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTEHEADERS_HPP
+#define EXECUTEHEADERS_HPP
+
+
+class ExecuteHeaders {
+public:
+	ExecuteHeaders();
+	virtual ~ExecuteHeaders();
+};
+
+
+#endif //EXECUTEHEADERS_HPP
