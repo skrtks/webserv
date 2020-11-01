10008d7ad9f93da138e5168da3c91e7a2ecd9772
pde-bakk <pde-bakk@student.codam.nl>
2020-11-01 22:11:02 +0100

    removed static function prototype

--- includes/Base64.hpp
+++ includes/Base64.hpp
@@ -6,7 +6,6 @@
 # define WEBSERV_BASE64_H
 # include <string>
 
-static inline bool is_base64(unsigned char c);
 std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len);
 std::string base64_decode(std::string const& encoded_string);
 #endif //WEBSERV_BASE64_H
