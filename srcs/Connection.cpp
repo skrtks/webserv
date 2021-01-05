397d2abc284c2e6da31f4d135c64091cf85790ca
Peer de Bakker <pde-bakk@student.codam.nl>
2021-01-05 13:44:11 +0100

    trying new socket options for macos

--- srcs/Connection.cpp
+++ srcs/Connection.cpp
@@ -68,16 +68,25 @@ Connection& Connection::operator= (const Connection &obj) {
 void Connection::setUpConnection() {
 	int socketFd;
 	int opt = 1;
+	struct timeval timeout;
+	timeout.tv_sec = 300;
+	timeout.tv_usec = 0;
 	for (std::vector<Server>::iterator it = _servers.begin(); it != _servers.end(); it++) {
 		ft_memset(&_serverAddr, 0, sizeof(_serverAddr)); // Clear struct from prev setup
 		if (!(socketFd = socket(AF_INET, SOCK_STREAM, 0)))
 			throw std::runtime_error(strerror(errno));
 
-		if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+		if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
 			throw std::runtime_error(strerror(errno));
 		}
-		if (fcntl(socketFd, F_SETFL, O_NONBLOCK) < 0)
+		if (setsockopt(socketFd, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) < 0) {
 			throw std::runtime_error(strerror(errno));
+		}
+		if (setsockopt(socketFd, SOL_SOCKET, SO_SNDTIMEO, (char*)&timeout, sizeof(timeout)) < 0) {
+			throw std::runtime_error(strerror(errno));
+		}
+//		if (fcntl(socketFd, F_SETFL, O_NONBLOCK) < 0)
+//			throw std::runtime_error(strerror(errno));
 		// Fill struct with info about port and ip
 		_serverAddr.sin_family = AF_INET; // ipv4
 		if (it->gethost() == "localhost") {
