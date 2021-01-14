8e457c1fa66dbd72226fb579e9f5fafe4d326922
Peer de Bakker <pde-bakk@student.codam.nl>
2021-01-14 21:53:18 +0100

    made CGI work with a php only module too

--- srcs/Cgi.cpp
+++ srcs/Cgi.cpp
@@ -34,7 +34,7 @@ Cgi::~Cgi() {
 	this->_m.clear();
 }
 
-void Cgi::populate_map(request_s &req, const std::string& OriginalUri) {
+void Cgi::populate_map(request_s &req, const std::string& OriginalUri, bool redirect_status) {
 	int split_path = req.uri.find_first_of('/', req.uri.find_first_of('.') );
 	char buf[500];
 	std::string	realpath = getcwd(buf, 500);
@@ -42,19 +42,22 @@ void Cgi::populate_map(request_s &req, const std::string& OriginalUri) {
 	this->_m["CONTENT_LENGTH"] = ft::inttostring(req.body.size());
 	this->_m["CONTENT_TYPE"] = req.headers[CONTENT_TYPE];
 	this->_m["GATEWAY_INTERFACE"] = "CGI/1.1";
-	this->_m["PATH_INFO"] = OriginalUri;
+	this->_m["PATH_INFO"] = OriginalUri + req.cgiparams;
 	this->_m["PATH_TRANSLATED"] = realpath + this->_m["PATH_INFO"];
-	this->_m["QUERY_STRING"] = req.uri.substr(req.uri.find_first_of('?') + 1);
+	this->_m["QUERY_STRING"] = req.cgiparams;
 	this->_m["REMOTE_ADDR"] = req.server->gethost();
 	this->_m["REMOTE_IDENT"] = "";
 	this->_m["REMOTE_USER"] = req.headers[REMOTE_USER];
 	this->_m["REQUEST_METHOD"] = req.MethodToSTring();
 	this->_m["REQUEST_URI"] = OriginalUri;
-	this->_m["SCRIPT_NAME"] = '.' + req.uri.substr(0, split_path - 1 );
+	this->_m["SCRIPT_FILENAME"] = req.uri.substr(1, split_path - 1 );
+	this->_m["SCRIPT_NAME"] = req.uri.substr(1, split_path - 1 );
 	this->_m["SERVER_NAME"] = req.server->getservername();
 	this->_m["SERVER_PORT"] = std::string(ft::inttostring(req.server->getport()));
 	this->_m["SERVER_PROTOCOL"] = "HTTP/1.1";
 	this->_m["SERVER_SOFTWARE"] = "HTTP 1.1";
+	if (redirect_status)
+		this->_m["REDIRECT_STATUS"] = "true";
 }
 
 void Cgi::map_to_env(request_s& request) {
@@ -66,6 +69,7 @@ void Cgi::map_to_env(request_s& request) {
 
 	for (std::map<std::string, std::string>::const_iterator it = _m.begin(); it != _m.end(); it++) {
 		std::string tmp = it->first + "=" + it->second;
+		std::cerr << _YELLOW "\t" << tmp << _END << std::endl;
 		this->_env[i] = ft_strdup(tmp.c_str());
 		if (!this->_env[i])
 			exit_fatal();
@@ -82,13 +86,23 @@ void	Cgi::clear_env() {
 	_env = NULL;
 }
 
+#include <sys/stat.h>
+
 int Cgi::run_cgi(request_s &request, std::string& scriptpath, const std::string& OriginalUri) {
 	int				incoming_file,
 					outgoing_file;
 	pid_t			pid;
-	char*			args[2] = {&scriptpath[0], NULL};
+	bool			redirect_status = false;
+	char*			args[3] = {&scriptpath[0], NULL, NULL};
 
-	this->populate_map(request, OriginalUri);
+	std::string phpcgipath = request.server->matchlocation(OriginalUri).getphpcgipath();
+	if (!phpcgipath.empty()) {
+		args[0] = ft_strdup(phpcgipath.c_str());
+		args[1] = ft_strdup(scriptpath.c_str());
+		redirect_status = true;
+	}
+
+	this->populate_map(request, OriginalUri, redirect_status);
 	this->map_to_env(request);
 
 	if ((incoming_file = open("/tmp/webservin.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRWXU)) == -1)
@@ -101,6 +115,7 @@ int Cgi::run_cgi(request_s &request, std::string& scriptpath, const std::string&
 		exit_fatal();
 
 	if (pid == 0) {
+		std::cerr << args[0] << " " << args[1] << (args[2] == NULL ? " NULL" : args[2]) << std::endl;
 		if ((outgoing_file = open("/tmp/webservout.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRWXU)) == -1)
 			exit_fatal();
 		if (dup2(outgoing_file, STDOUT_FILENO) == -1 || close(outgoing_file) == -1)
@@ -109,7 +124,7 @@ int Cgi::run_cgi(request_s &request, std::string& scriptpath, const std::string&
 			exit_fatal();
 		if (dup2(incoming_file, STDIN_FILENO) == -1 || close(incoming_file) == -1)
 			exit_fatal();
-		if (execve(scriptpath.c_str(), args, _env) == -1)
+		if (execve(args[0], args, _env) == -1)
 			std::cerr << "execve: " << strerror(errno) << std::endl;
 		exit(EXIT_FAILURE);
 	}
@@ -118,5 +133,9 @@ int Cgi::run_cgi(request_s &request, std::string& scriptpath, const std::string&
 	waitpid(0, NULL, 0);
 	if ((outgoing_file = open("/tmp/webservout.txt", O_RDONLY, S_IRWXU)) == -1)
 		exit_fatal();
+	if (!phpcgipath.empty()) {
+		free(args[0]);
+		free(args[1]);
+	}
 	return (outgoing_file);
 }
